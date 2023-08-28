722ab0cfd2e014820642628fe66ade18627ddbb5
Andrii L <99610122+Lap-DevOps@users.noreply.github.com>
2023-08-28 15:52:27 +0100

    -- add forms init --

--- forms.py
+++ forms.py
@@ -1,7 +1,8 @@
+from flask import url_for
 from flask_wtf import FlaskForm, RecaptchaField
+from markupsafe import Markup
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo, Length
-from wtforms import ValidationError
 
 from models import User
 
@@ -11,28 +12,30 @@ class RegistrationForm(FlaskForm):
     email = StringField('Email',
                         validators=[DataRequired(), Length(min=2, max=50, message='Email is too short'), Email()])
     password = PasswordField('Password',
-                             validators=[DataRequired(), EqualTo('confirm_password', message='Passwords must match')])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired()])
-    accept_tos = BooleanField('I accept the <a href=''>TOS</a>', validators=[DataRequired()], default=False)
-    recaptcha = RecaptchaField()
+                             validators=[DataRequired(), Length(min=2, max=20, message='Password is too short')])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('confirm_password',
+                                                                                             message='Passwords must match')])
+    accept_tos = BooleanField(Markup('I accept the <a href="">TOS</a> -'), validators=[DataRequired()], default=False)
+    # recaptcha = RecaptchaField()
     submit = SubmitField('Submit')
 
     def __init__(self, *args, **kwargs):
         super(RegistrationForm, self).__init__(*args, **kwargs)
+        self.accept_tos.label.text = Markup("I accept the <a href='{}'>TOS</a>".format(url_for('home')))
 
-    def validate(self):
-        initial_validation = super(RegistrationForm, self).validate()
-        if not initial_validation:
-            return False
-        user = User.query.filter_by(username=self.username.data).first()
-        if user:
-            self.username.errors.append("Username already registered")
-            return False
-        user = User.query.filter_by(email=self.email.data).first()
-        if user:
-            self.email.errors.append("Email already registered")
-            return False
-        return True
+    # def validate(self):
+    #     initial_validation = super(RegistrationForm, self).validate()
+    #     if not initial_validation:
+    #         return False
+    #     user = User.query.filter_by(username=self.username.data).first()
+    #     if user:
+    #         self.username.errors.append("Username already registered")
+    #         return False
+    #     user = User.query.filter_by(email=self.email.data).first()
+    #     if user:
+    #         self.email.errors.append("Email already registered")
+    #         return False
+    #     return True
 
 
 class LoginForm(FlaskForm):
@@ -45,15 +48,15 @@ class LoginForm(FlaskForm):
     def __init__(self, *args, **kwargs):
         super(LoginForm, self).__init__(*args, **kwargs)
 
-    def validate(self):
-        initial_validation = super(LoginForm, self).validate()
-        if not initial_validation:
-            return False
-        user = User.query.filter_by(email=self.email.data).first()
-        if not user:
-            self.email.errors.append('Unknown email')
-            return False
-        if not user.verify_password(self.password.data):
-            self.password.errors.append('Invalid password')
-            return False
-        return True
+    # def validate(self):
+    #     initial_validation = super(LoginForm, self).validate()
+    #     if not initial_validation:
+    #         return False
+    #     user = User.query.filter_by(email=self.email.data).first()
+    #     if not user:
+    #         self.email.errors.append('Unknown email')
+    #         return False
+    #     if not user.verify_password(self.password.data):
+    #         self.password.errors.append('Invalid password')
+    #         return False
+    #     return True
