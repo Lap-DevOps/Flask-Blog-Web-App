109c79a26dd9528ccd2026b48c98160d7b296cc6
Andrii L <99610122+Lap-DevOps@users.noreply.github.com>
2023-08-30 13:26:19 +0100

    -- add some macro for register form

--- forms.py
+++ forms.py
@@ -4,19 +4,16 @@ from markupsafe import Markup
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, EmailField
 from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
 
-from models import User
-
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=4, max=10, message='Name is too short')])
     email = EmailField('Email',
                        validators=[DataRequired(), Length(min=2, max=50, message='Email is too short'), Email()])
     password = PasswordField('Password',
-                             validators=[DataRequired(), Length(min=2, max=20, message='Password is too short')])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(),
-
-                                                                     EqualTo('password',
-                                                                             message='Passwords must match')])
+                             validators=[DataRequired(), Length(min=2, max=20, message='Password is too short'),
+                                         EqualTo('confirm_password', message='Passwords must match')])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password', message='Passwords must match')])
     accept_tos = BooleanField(Markup('I accept the <a href="">TOS</a> -'), validators=[DataRequired()], default=False)
     recaptcha = RecaptchaField()
     submit = SubmitField('Submit')
@@ -25,16 +22,21 @@ class RegistrationForm(FlaskForm):
         super(RegistrationForm, self).__init__(*args, **kwargs)
         self.accept_tos.label.text = Markup("I accept the <a href='{}'>TOS</a>".format(url_for('home')))
 
+    def validate_username(self, username):
+        excluded_chars = " *?!'^+%&/()=}][{$#"
+        for char in self.username.data:
+            if char in excluded_chars:
+                raise ValidationError(
+                    f"Character {char} is not allowed in username.")
+
     # def validate_email(self, field):
     #     if User.query.filter_by(email=field.data).first():
-    #         raise ValidationError('This email is already registered. Please use a different one.')
+    #         raise ValidationError(
+    #               'This email is already registered. Please use a different one.')
     #
     # def validate_username(self, field):
     #     if User.query.filter_by(username=field.data).first():
     #         raise ValidationError('This username is already taken. Please choose a different one.')
-
-
-
 
 
 class LoginForm(FlaskForm):
