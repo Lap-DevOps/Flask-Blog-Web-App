66c174170c0dcab0489e2ffe9e6027b3ebd20bb9
Andrii L <99610122+Lap-DevOps@users.noreply.github.com>
2023-08-30 13:29:58 +0100

    -- add some macro for register/login form

--- forms.py
+++ forms.py
@@ -40,7 +40,7 @@ class RegistrationForm(FlaskForm):
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Email',
+    email = EmailField('Email',
                         validators=[DataRequired(), Length(1, 64), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Keep me logged in')
