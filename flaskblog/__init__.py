b97b0fc48370dde629673c1f361380be18d78b19
Andrii L <99610122+Lap-DevOps@users.noreply.github.com>
2023-09-16 11:02:28 +0100

    -- add mail resset password feature

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -4,6 +4,7 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_moment import Moment
+from flask_mail import Mail
 
 from config import Configuration
 
@@ -17,6 +18,9 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+
+
+mail = Mail(app)
 
 moment = Moment(app)
 
