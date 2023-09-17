d42d513bbebe428a14041a1166ef2194c6c01854
Andrii L <99610122+Lap-DevOps@users.noreply.github.com>
2023-09-17 18:10:38 +0100

    -- add pattern app factory

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,13 +1,12 @@
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-from flask_migrate import Migrate
-from flask_sqlalchemy import SQLAlchemy
-from flask_moment import Moment
 from flask_mail import Mail
+from flask_migrate import Migrate
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
 
 from config import Configuration
-
 
 # Initialize Flask-SQLAlchemy
 db = SQLAlchemy()
@@ -16,15 +15,15 @@ login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 
-
 mail = Mail()
 moment = Moment()
 migrate = Migrate(db)
 
-
 from flaskblog.main.routes import main
 from flaskblog.users.routes import users
 from flaskblog.posts.routes import posts
+
+
 # from flaskblog.users.utils import *
 
 
@@ -38,7 +37,6 @@ def crate_app(config_class=Configuration):
     moment.init_app(app)
     migrate.init_app(app)
     mail.init_app(app)
-
 
     app.register_blueprint(main)
     app.register_blueprint(users)
